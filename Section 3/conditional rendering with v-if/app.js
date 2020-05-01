122e8c64abbde9e22c03c373cf1e47c6d5fbb83a
Kristof Berge <kristof.berge@gmail.com>
2020-05-01 15:33:47 -0300

    Section 3

---
+++ Section 3/conditional rendering with v-if/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    show: true
+  },
+  methods: {
+    
+    },
+  computed: {
+
+  }
+});
---
+++ Section 3/rendering lists/app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      {name: 'Max', age: 27, color: 'red'},
+      {name: 'Kristof', age: 26, color: 'blue'}
+    ]
+  },
+  methods: {
+    
+    },
+  computed: {
+
+  }
+});
