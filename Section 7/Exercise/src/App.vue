d0678b79d7886ef1a864089f9ea9f687642c23b3
Kristof Berge <kristof.berge@gmail.com>
2020-05-02 19:06:19 -0300

    Section 7

---
+++ Section 7/Exercise/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <overview></overview>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from './components/shared/Header.vue';
+import Overview from './components/server/Overview.vue';
+import Footer from './components/shared/Footer.vue';
+export default {
+    components: {
+        appHeader: Header,
+        Overview,
+        'app-footer': Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Section 7/Exercise/src/components/server/Overview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <servers-statusses></servers-statusses>
+    <bottom-message></bottom-message>
+  </div>
+</template>
+
+<script>
+import Message from './Message.vue';
+import ServerStatus from './ServerStatus.vue';
+export default {
+    components: {
+        'bottom-message': Message,
+        'servers-statusses': ServerStatus
+    }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
