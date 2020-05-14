c942458d62eef89cb5b86ad1c837e0828d4a0402
krsitof_berge <kristof.berge@granter.com.br>
2020-05-14 09:47:02 -0300

    Section 11

---
+++ Section 11/Exercise/src/FullnameInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <label for="firstName">First name</label>
+    <input id="firstName" type="text" @input="updateValue" v-model="firstName" />
+    <label for="lastName">Last name</label>
+    <input id="lastName" type="text" @input="updateValue" v-model="lastName" />
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    data() {
+        return {
+            firstName: '',
+            lastName: ''
+        }
+    },
+    methods: {
+        updateValue() {
+            this.$emit('input', this.firstName + ' ' + this.lastName);
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
